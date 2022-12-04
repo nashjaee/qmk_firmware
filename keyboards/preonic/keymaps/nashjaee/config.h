86309c24e7ab1995a38c683e0b4c6ce2f7bc832d
nashjaee <nema.ashjaee@gmail.com>
2022-12-04 13:17:25 -0800

    ignore mod tap interrupt

--- keyboards/preonic/keymaps/nashjaee/config.h
+++ keyboards/preonic/keymaps/nashjaee/config.h
@@ -46,3 +46,6 @@
 
 /* override number of MIDI tone keycodes (each octave adds 12 keycodes and allocates 12 bytes) */
 //#define MIDI_TONE_KEYCODE_OCTAVES 2
+
+#define TAPPING_TERM 200
+#define IGNORE_MOD_TAP_INTERRUPT
